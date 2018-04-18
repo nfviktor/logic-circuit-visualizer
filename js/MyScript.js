ea8eb397e1bdfd6f0a3a5948bcc9cb66cb6041c8
Viktor Nagy Farkas <nfviktor93@gmail.com>
2018-04-18 20:16:49 +0200

    Cleaner code, tried out some stuff

--- js/MyScript.js
+++ js/MyScript.js
@@ -6,61 +6,72 @@ $(function(){
 	var camera = new THREE.PerspectiveCamera(30,window.innerWidth/window.innerHeight, 1, 1000);
 	var renderer = new THREE.WebGLRenderer({antialias: true});
 	var controls = new THREE.OrbitControls( camera );
+	controls.maxPolarAngle = Math.PI / 2;
 
-	renderer.setClearColor( 0x333333 );
+	renderer.setClearColor( 0x00BFFF );
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.soft = true;
 
-	// custom global variables
+// custom global variables
 	var raycaster = new THREE.Raycaster();
 	var objects = [];
 	var mouse = new THREE.Vector2();
 
+	init();
 	animate();
 
-	var particleMaterial;
-	particleMaterial = new THREE.SpriteMaterial( {
-		color: 0xff0000,
-	});
-
-	
+// Initialize
+	function init(){
 	renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-//--------------------------LIGHTS ------------------------------------------------
-
-	var light = new THREE.HemisphereLight( 0xffffff, 0xeeeeee, 0.75 );
+	window.addEventListener( 'resize', onWindowResize, false );
+	
+	//LIGHTS
+	/*
+	var light = new THREE.HemisphereLight( 0x00BFFF, 0xeeeeee, 0.75 );
   	light.position.set( 0.5, 1, 0.75 );
-  	scene.add( light );
-
-  	var lightAmbient = new THREE.AmbientLight( 0xAAAAAA ); // soft white light
+  	scene.add( light );*/
+	
+	var lightAmbient = new THREE.AmbientLight( 0xAAAAAA ); // soft white light
 	scene.add( lightAmbient );
-
+	
 	var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
 	//spotLight.castShadow = true;
 	directionalLight.position.set(1,1,1);
 	scene.add(directionalLight);
 
-//----------------------------------------HELPERS ------------------------------------
-	var axis = new THREE.AxesHelper(10);
+	//HELPERS
+	var axis = new THREE.AxesHelper(5);
+	axis.position.y = -3;
 	scene.add(axis);
 
-	var grid = new THREE.GridHelper(50, 5 , "rgb(255,0,0)");
-	//scene.add(grid);
-//----------------------------------Camera---------------------
-	camera.position.set( 100, 100, 100 );
+	var grid = new THREE.GridHelper(100, 20 , "rgb(255,0,0)");
+	grid.position.y = -3;
+	scene.add(grid);
+	
+	//Camera
+	camera.position.set( 80, 80, 80 );
 	camera.lookAt(scene.position);
 	controls.update();
 
-//------------------------Plane geometry------------------------
-	var planeGeometry = new THREE.PlaneGeometry(60,60,60);
-	var planeMaterial = new THREE.MeshLambertMaterial({color:0x555555});
+	//teszt drag controls
+	/*
+	var dragControls = new THREE.DragControls( objects, camera, renderer.domElement );
+	dragControls.addEventListener( 'dragstart', function ( event ) { controls.enabled = false; } );
+	dragControls.addEventListener( 'dragend', function ( event ) { controls.enabled = true; } );
+	*/
+
+	//Plane geometry
+	var planeGeometry = new THREE.PlaneGeometry(1000,1000,1000);
+	var planeMaterial = new THREE.MeshLambertMaterial({color:0x693109});
 	var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+	plane.position.y = -3;
 	plane.rotation.x = -0.5*Math.PI;
 	plane.receiveShadow = true;
-
 	scene.add(plane);
-//--------------------------------------- A LÉNYEG (import része) ---------------------------------------
+
+	//-import rész
 	var loader = new THREE.JSONLoader();
 	// zarojelbe 1. az eleresi ut, a 2. a callback function!
 	//loader.load('./model/Wood2.json', addModel);
@@ -74,9 +85,8 @@ $(function(){
 		scene.add(object);
 		objects.push(object);
 	}
-	
-	//----------------------------END OF Import!-------------------------------------------------------------
 
+	//-Gombok Eventje
 	document.getElementById("add_negator").onclick = function() {
 		loader.load('./model/Negator.json', addModel); 
 	};
@@ -100,22 +110,18 @@ $(function(){
 	document.getElementById("add_xorgate").onclick = function() {
 		loader.load('./model/TwoInput.json', addModel); 
 	};
-	//-------------Remove Button----------------
-
+	
 	document.getElementById("remove").onclick = function() {
 
 	};
 
-
-	//-------------------
 	document.getElementById("edit").onclick = function() {
 
 	};
 	
+}
 
-//-------------------------------------Mouse Events----------------------------------------------------------------------
-
-
+//Mouse Events
 function onDocumentMouseMove( event ) {
 				event.preventDefault();
 				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -132,36 +138,36 @@ function onDocumentMouseDown( event ) {
 					console.log("THERE!!!");
 				}
 				console.log(mouse);
-			}		
+			}	
 
+function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}	
 
-
-//---------------------------------------------------------------------------------
-
-
-	function animate() {
+//animate function
+function animate() {
     	requestAnimationFrame( animate );
 		render();		
 		update();
-	}
+}
 
-	function update(){	
+function update(){	
 		controls.update();
-	}
+}
 
-	function render() {
+function render() {
 		// find intersections
-
-				raycaster.setFromCamera( mouse, camera );
-				var intersects = raycaster.intersectObjects( objects );
-				if ( intersects.length > 0 ) {
-					console.log("there");
-				}
-
+		raycaster.setFromCamera( mouse, camera );
+		var intersects = raycaster.intersectObjects( objects );
+		if ( intersects.length > 0 ) {
+			console.log("there");
+		}
 		renderer.render( scene, camera );
-	}
+}
 	
 
-	$("#webGL-container").append(renderer.domElement);
-	renderer.render(scene,camera);
+$("#webGL-container").append(renderer.domElement);
+renderer.render(scene,camera);
 });
